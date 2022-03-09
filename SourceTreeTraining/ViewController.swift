315cc8a92c999e65a401031282262205149e15c9
İlker Özcan <AdnanIlker.Ozcan@sekerbank.com.tr>
2022-03-09 13:12:13 +0300

    Inital commit.

---
+++ SourceTreeTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SourceTreeTraining
+//
+//  Created by İlker Özcan - ERP ve Dijital Bank. BT Yön. / Yazılım Geliştirme Müdürlüğü on 9.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
